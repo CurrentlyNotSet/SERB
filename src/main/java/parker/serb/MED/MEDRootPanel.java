b84cec14139c0ea8058682d77a771298434d3321
Parker Johnston <parkjohnston@gmail.com>
2016-12-28 09:44:01 -0500

    completed activity removal in all sections

--- src/main/java/parker/serb/MED/MEDRootPanel.java
+++ src/main/java/parker/serb/MED/MEDRootPanel.java
@@ -10,6 +10,7 @@ import javax.swing.JTabbedPane;
 import javax.swing.event.ChangeEvent;
 import parker.serb.activity.ActivityPanel;
 import parker.serb.Global;
+import parker.serb.activity.RemoveActivityEntryDialog;
 import parker.serb.party.PartiesPanel;
 import parker.serb.party.PartySearchDialog;
 import parker.serb.sql.MEDCaseSearchData;
@@ -248,6 +249,16 @@ public class MEDRootPanel extends javax.swing.JPanel {
         CancelUpdate cancel;
         switch (jTabbedPane1.getTitleAt(jTabbedPane1.getSelectedIndex())) {
             case "Activity":
+                new RemoveActivityEntryDialog(
+                    Global.root,
+                    true,
+                    activityPanel1.getActvityTable().getValueAt
+                    (
+                        activityPanel1.getActvityTable().getSelectedRow(),
+                        5
+                    ).toString()
+                );
+                activityPanel1.loadAllActivity();
                 break;
             case "Parties":
                 partiesPanel1.removeParty();
