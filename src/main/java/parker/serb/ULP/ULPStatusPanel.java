38cec25909fa8f918ba16481efe68d6796c23b6d
Parker Johnston <parkjohnston@gmail.com>
2017-02-07 22:29:36 -0500

    fixed ulp panel issue

--- src/main/java/parker/serb/ULP/ULPStatusPanel.java
+++ src/main/java/parker/serb/ULP/ULPStatusPanel.java
@@ -1111,7 +1111,7 @@ public class ULPStatusPanel extends javax.swing.JPanel {
                                                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                             .addGroup(layout.createSequentialGroup()
-                                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 519, javax.swing.GroupLayout.PREFERRED_SIZE)
                                                 .addContainerGap())
                                         );
                                     }// </editor-fold>//GEN-END:initComponents
