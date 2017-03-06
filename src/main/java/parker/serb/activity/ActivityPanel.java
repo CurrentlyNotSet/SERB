1f0b9afee6056daef3d2161b1d83d6c480c5bc41
altrdgenetics <tofudeliveryae86@hotmail.com>
2017-03-06 14:44:05 -0500

    Completed T029 & T030 Enhancements as well as fixed the CMDS adding new case. This was supposed to be fixed earlier but was not.

--- src/main/java/parker/serb/activity/ActivityPanel.java
+++ src/main/java/parker/serb/activity/ActivityPanel.java
@@ -35,7 +35,7 @@ public class ActivityPanel extends javax.swing.JPanel {
 
     List activty;
     ImageIcon aboutIcon = new ImageIcon(getClass().getResource("/file-icon.png"));
-    
+
     /**
      * Creates new form ActivityPanel
      */
@@ -44,7 +44,7 @@ public class ActivityPanel extends javax.swing.JPanel {
         setTableColumnWidths();
         addListeners();
     }
-    
+
     private void addListeners() {
         actvityTable.setDefaultRenderer(Object.class, new TableCellRenderer(){
             private DefaultTableCellRenderer DEFAULT_RENDERER =  new DefaultTableCellRenderer();
@@ -58,7 +58,7 @@ public class ActivityPanel extends javax.swing.JPanel {
                 return c;
             }
         });
-        
+
         searchTextBox.getDocument().addDocumentListener(new DocumentListener() {
 
             @Override
@@ -76,7 +76,7 @@ public class ActivityPanel extends javax.swing.JPanel {
                 loadActivity(searchTextBox.getText().trim());
             }
         });
-        
+
         actvityTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
@@ -88,15 +88,15 @@ public class ActivityPanel extends javax.swing.JPanel {
             }
         });
 
-                
+
         actvityTable.addMouseListener(new MouseListener() {
 
             @Override
             public void mouseClicked(MouseEvent e) {
                 if(actvityTable.getSelectedRow() >= 0) {
-                    String filePath = actvityTable.getValueAt(actvityTable.getSelectedRow(), 4).toString();
+                    String filePath = actvityTable.getValueAt(actvityTable.getSelectedRow(), 5).toString();
 
-                    if (e.getClickCount() == 2 && !filePath.equals("") && actvityTable.getSelectedColumn() == 4 && e.getButton() == MouseEvent.BUTTON1) {
+                    if (e.getClickCount() == 2 && !filePath.equals("") && actvityTable.getSelectedColumn() == 5 && e.getButton() == MouseEvent.BUTTON1) {
 
                         switch (Global.activeSection) {
                             case "ORG":
@@ -112,11 +112,11 @@ public class ActivityPanel extends javax.swing.JPanel {
                                 FileService.openFile(filePath);
                                 break;
                         }
-                    } else if (e.getClickCount() == 2 && actvityTable.getSelectedColumn() != 4 && e.getButton() == MouseEvent.BUTTON1) {
-                        Audit.addAuditEntry("Viewing Activty Detail for ID: " + actvityTable.getValueAt(actvityTable.getSelectedRow(), 5).toString());
+                    } else if (e.getClickCount() == 2 && actvityTable.getSelectedColumn() != 5 && e.getButton() == MouseEvent.BUTTON1) {
+                        Audit.addAuditEntry("Viewing Activty Detail for ID: " + actvityTable.getValueAt(actvityTable.getSelectedRow(), 6).toString());
                         new DetailedActivityDialog((JFrame) Global.root.getRootPane().getParent(),
                                 true,
-                                actvityTable.getValueAt(actvityTable.getSelectedRow(), 5).toString());
+                                actvityTable.getValueAt(actvityTable.getSelectedRow(), 6).toString());
                         loadAllActivity();
                     }
                 }
@@ -135,130 +135,155 @@ public class ActivityPanel extends javax.swing.JPanel {
             public void mouseExited(MouseEvent e) {}
         });
     }
-    
+
     /**
      * Set the width of all the columns in the table.
      */
     private void setTableColumnWidths() {
+        //Date
         actvityTable.getColumnModel().getColumn(0).setPreferredWidth(175);
         actvityTable.getColumnModel().getColumn(0).setMinWidth(175);
         actvityTable.getColumnModel().getColumn(0).setMaxWidth(175);
-        actvityTable.getColumnModel().getColumn(3).setPreferredWidth(200);
-        actvityTable.getColumnModel().getColumn(3).setMinWidth(200);
-        actvityTable.getColumnModel().getColumn(3).setMaxWidth(200);
-        actvityTable.getColumnModel().getColumn(4).setPreferredWidth(25);
-        actvityTable.getColumnModel().getColumn(4).setMinWidth(25);
-        actvityTable.getColumnModel().getColumn(4).setMaxWidth(25);
-        actvityTable.getColumnModel().getColumn(5).setPreferredWidth(0);
-        actvityTable.getColumnModel().getColumn(5).setMinWidth(0);
-        actvityTable.getColumnModel().getColumn(5).setMaxWidth(0);
+        //User
+        actvityTable.getColumnModel().getColumn(4).setPreferredWidth(200);
+        actvityTable.getColumnModel().getColumn(4).setMinWidth(200);
+        actvityTable.getColumnModel().getColumn(4).setMaxWidth(200);
+        // Document Icon
+        actvityTable.getColumnModel().getColumn(5).setPreferredWidth(25);
+        actvityTable.getColumnModel().getColumn(5).setMinWidth(25);
+        actvityTable.getColumnModel().getColumn(5).setMaxWidth(25);
+        // ID
+        actvityTable.getColumnModel().getColumn(6).setPreferredWidth(0);
+        actvityTable.getColumnModel().getColumn(6).setMinWidth(0);
+        actvityTable.getColumnModel().getColumn(6).setMaxWidth(0);
     }
 
     /**
-     * Load all activity for the currently selected case.  Takes a term as a 
+     * Load all activity for the currently selected case.  Takes a term as a
      * parameter, but is not required.
      * @param searchTerm a string of a value, if blank pass ""
      */
     private void loadActivity(String searchTerm) {
-        
+
         Audit.addAuditEntry("Searched Activty for " + searchTerm);
-        
+
         DefaultTableModel model = (DefaultTableModel) actvityTable.getModel();
         model.setRowCount(0);
-        
+
         for (Object activty1 : activty) {
             Activity act = (Activity) activty1;
             if(act.action.toLowerCase().contains(searchTerm.toLowerCase())
+                    || act.from.toLowerCase().contains(searchTerm.toLowerCase())
                     || act.user.toLowerCase().contains(searchTerm.toLowerCase())) {
                 if(act.fileName == null) {
-                    model.addRow(new Object[] {act.date, act.action, act.comment, act.user, "", act.id});
+                    if (!documentsOnlyCheckbox.isSelected()){
+                        model.addRow(new Object[] {act.date, act.action, act.comment, act.from, act.user, "", act.id});
+                    }
                 } else {
-                    model.addRow(new Object[] {act.date, act.action, act.comment, act.user, aboutIcon, act.id});
-                } 
+                    model.addRow(new Object[] {act.date, act.action, act.comment, act.from, act.user, aboutIcon, act.id});
+                }
             }
         }
-        
+
         if(activty.isEmpty()) {
             searchTextBox.setEnabled(false);
+            documentsOnlyCheckbox.setEnabled(false);
+            clearSearchButton.setEnabled(false);
         } else {
             searchTextBox.setEnabled(true);
+            documentsOnlyCheckbox.setEnabled(true);
+            clearSearchButton.setEnabled(true);
         }
     }
-    
+
     /**
      * clears the search box and removes all results displayed from the table
      */
     public void clearAll() {
         searchTextBox.setEnabled(false);
+        documentsOnlyCheckbox.setEnabled(false);
+        clearSearchButton.setEnabled(false);
         searchTextBox.setText("");
         DefaultTableModel model = (DefaultTableModel) actvityTable.getModel();
         model.setRowCount(0);
     }
-    
+
     /**
      * loads all activity non limited for the currently selected case
      * Will be removed in future releases
      */
     public void loadAllActivity() {
-        
+
         searchTextBox.setText("");
-        
+
         DefaultTableModel model = (DefaultTableModel) actvityTable.getModel();
         model.setRowCount(0);
-        
+
         activty = Activity.loadCaseNumberActivity("%");
-        
+
         for (Object activty1 : activty) {
             Activity act = (Activity) activty1;
-            
+
             if(act.fileName == null) {
-                model.addRow(new Object[] {act.date, act.action, act.comment, act.user, "", act.id});
+                if (!documentsOnlyCheckbox.isSelected()){
+                    model.addRow(new Object[] {act.date, act.action, act.comment, act.from, act.user, "", act.id});
+                }
             } else {
-                actvityTable.getColumnModel().getColumn(4).setCellRenderer(new ImageRenderer());
-                model.addRow(new Object[] {act.date, act.action, act.comment, act.user, act.fileName.trim(), act.id});
-            }      
+                actvityTable.getColumnModel().getColumn(5).setCellRenderer(new ImageRenderer());
+                model.addRow(new Object[] {act.date, act.action, act.comment, act.from, act.user, act.fileName.trim(), act.id});
+            }
         }
-        
+
         if(activty.isEmpty()) {
+            documentsOnlyCheckbox.setEnabled(false);
             searchTextBox.setEnabled(false);
+            clearSearchButton.setEnabled(false);
         } else {
+            documentsOnlyCheckbox.setEnabled(true);
             searchTextBox.setEnabled(true);
+            clearSearchButton.setEnabled(true);
         }
     }
-    
+
     public void loadAllHearingActivity() {
-        
+
         searchTextBox.setText("");
-        
+
         DefaultTableModel model = (DefaultTableModel) actvityTable.getModel();
         model.setRowCount(0);
-        
+
         if(HearingCase.getBoardActionPCDate() != null) {
             activty = Activity.loadHearingActivity();
-        
+
             for (Object activty1 : activty) {
                 Activity act = (Activity) activty1;
 
                 if(act.fileName == null) {
-                    model.addRow(new Object[] {act.date, act.action, act.comment, act.user, "", act.id});
+                    if (!documentsOnlyCheckbox.isSelected()){
+                        model.addRow(new Object[] {act.date, act.action, act.comment, act.from, act.user, "", act.id});
+                    }
                 } else {
-                    actvityTable.getColumnModel().getColumn(4).setCellRenderer(new ImageRenderer());
-                    model.addRow(new Object[] {act.date, act.action, act.comment, act.user, act.fileName.trim(), act.id});
-                }      
+                    actvityTable.getColumnModel().getColumn(5).setCellRenderer(new ImageRenderer());
+                    model.addRow(new Object[] {act.date, act.action, act.comment, act.from, act.user, act.fileName.trim(), act.id});
+                }
             }
         }
-        
+
         if(activty.isEmpty()) {
+            documentsOnlyCheckbox.setEnabled(false);
             searchTextBox.setEnabled(false);
+            clearSearchButton.setEnabled(false);
         } else {
+            documentsOnlyCheckbox.setEnabled(true);
             searchTextBox.setEnabled(true);
+            clearSearchButton.setEnabled(true);
         }
     }
 
     public JTable getActvityTable() {
         return actvityTable;
     }
-    
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -273,6 +298,7 @@ public class ActivityPanel extends javax.swing.JPanel {
         jScrollPane1 = new javax.swing.JScrollPane();
         actvityTable = new javax.swing.JTable();
         clearSearchButton = new javax.swing.JButton();
+        documentsOnlyCheckbox = new javax.swing.JCheckBox();
 
         jLabel1.setText("Search:");
 
@@ -281,14 +307,14 @@ public class ActivityPanel extends javax.swing.JPanel {
 
             },
             new String [] {
-                "Date", "Activity", "Comments", "User", "", "id"
+                "Date", "Activity", "Comments", "From", "User", "", "id"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Byte.class, java.lang.Object.class
+                java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Byte.class, java.lang.Object.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false
+                false, false, false, false, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -302,14 +328,21 @@ public class ActivityPanel extends javax.swing.JPanel {
         actvityTable.getTableHeader().setReorderingAllowed(false);
         jScrollPane1.setViewportView(actvityTable);
         if (actvityTable.getColumnModel().getColumnCount() > 0) {
-            actvityTable.getColumnModel().getColumn(4).setResizable(false);
             actvityTable.getColumnModel().getColumn(5).setResizable(false);
+            actvityTable.getColumnModel().getColumn(6).setResizable(false);
         }
 
         clearSearchButton.setText("Clear");
         clearSearchButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 clearSearchButtonActionPerformed(evt);
+            }
+        });
+
+        documentsOnlyCheckbox.setText("Documents Only");
+        documentsOnlyCheckbox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                documentsOnlyCheckboxActionPerformed(evt);
             }
         });
 
@@ -323,6 +356,8 @@ public class ActivityPanel extends javax.swing.JPanel {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(searchTextBox)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(documentsOnlyCheckbox)
+                .addGap(18, 18, 18)
                 .addComponent(clearSearchButton)
                 .addContainerGap())
             .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 719, Short.MAX_VALUE)
@@ -334,7 +369,8 @@ public class ActivityPanel extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(searchTextBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(clearSearchButton))
+                    .addComponent(clearSearchButton)
+                    .addComponent(documentsOnlyCheckbox))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 279, Short.MAX_VALUE))
         );
@@ -342,18 +378,24 @@ public class ActivityPanel extends javax.swing.JPanel {
 
     private void clearSearchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearSearchButtonActionPerformed
         Audit.addAuditEntry("Clicked Activity Clear Button");
+        documentsOnlyCheckbox.setSelected(false);
         searchTextBox.setText("");
     }//GEN-LAST:event_clearSearchButtonActionPerformed
+
+    private void documentsOnlyCheckboxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_documentsOnlyCheckboxActionPerformed
+        loadActivity(searchTextBox.getText().trim());
+    }//GEN-LAST:event_documentsOnlyCheckboxActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTable actvityTable;
     private javax.swing.JButton clearSearchButton;
+    private javax.swing.JCheckBox documentsOnlyCheckbox;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField searchTextBox;
     // End of variables declaration//GEN-END:variables
 
-    
+
     class ImageRenderer extends DefaultTableCellRenderer {
         JLabel lbl = new JLabel();
 
@@ -362,13 +404,13 @@ public class ActivityPanel extends javax.swing.JPanel {
         @Override
         public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
             boolean hasFocus, int row, int column) {
-            
+
             if(!value.equals("")) {
                 lbl.setIcon(icon);
             } else {
                 lbl.setIcon(null);
             }
-            
+
             if (!isSelected) {
                     lbl.setBackground(row % 2 == 0 ? Color.WHITE : Global.ALTERNATE_ROW_COLOR);
             } else {
