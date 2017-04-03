bc1f7170788c15dccc38b428c6e13ba2d6385048
Parker Johnston <parkjohnston@gmail.com>
2017-04-02 21:46:20 -0400

    removed party table sort

--- src/main/java/parker/serb/party/PartiesPanel.java
+++ src/main/java/parker/serb/party/PartiesPanel.java
@@ -3496,7 +3496,6 @@ public class PartiesPanel extends javax.swing.JPanel {
 
         jLabel1.setText("Search:");
 
-        jTable1.setAutoCreateRowSorter(true);
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
