aee763e377fd6077fd1fcf5ec7163601c65d51a2
altrdgenetics <tofudeliveryae86@hotmail.com>
2017-04-11 13:01:14 -0400

    Completed: (PROD039) - New Case Notification for REP Cases. #402

--- src/main/java/parker/serb/sql/DocketNotifications.java
+++ src/main/java/parker/serb/sql/DocketNotifications.java
@@ -12,16 +12,16 @@ import parker.serb.util.SlackNotification;
  * @author parkerjohnston
  */
 public class DocketNotifications {
-    
+
     public int id;
     public String section;
     public String sendTo;
     public String messageSubject;
     public String messageBody;
-    
+
     public static void addNotification(String caseNumber, String section, int mediatorID) {
         Statement stmt = null;
-            
+
         try {
             stmt = Database.connectToDB().createStatement();
 
@@ -38,7 +38,32 @@ public class DocketNotifications {
             SlackNotification.sendNotification(ex);
             if(ex.getCause() instanceof SQLServerException) {
                 addNotification(caseNumber, section, mediatorID);
-            } 
+            }
+        } finally {
+            DbUtils.closeQuietly(stmt);
+        }
+    }
+
+    public static void addNewCaseNotification(String caseNumber, String section, int userID) {
+        Statement stmt = null;
+
+        try {
+            stmt = Database.connectToDB().createStatement();
+
+            String sql = "Insert INTO DocketNotifications VALUES (?,?,?,?)";
+
+            PreparedStatement preparedStatement = stmt.getConnection().prepareStatement(sql);
+            preparedStatement.setString(1, section);
+            preparedStatement.setString(2, User.getEmailByID(userID));
+            preparedStatement.setString(3, "New Case Created: " + caseNumber);
+            preparedStatement.setString(4, "A new case was created: " + caseNumber + ".\n\nPlease check SMDS for more information.");
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException ex) {
+            SlackNotification.sendNotification(ex);
+            if(ex.getCause() instanceof SQLServerException) {
+                addNotification(caseNumber, section, userID);
+            }
         } finally {
             DbUtils.closeQuietly(stmt);
         }
